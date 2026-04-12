0bcbee639174ccf74bae845ccc03c5c74705d7a0
v.litovkin <vital.litovk@gmail.com>
2026-04-12 17:18:18 +0300

    Remove legacy files not present on factura-v2

--- src/pages/robots.txt.ts
+++
@@ -1,6 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = () =>
-  new Response(['User-agent: *', 'Allow: /', 'Host: https://factura78.ru', 'Sitemap: https://factura78.ru/sitemap-index.xml'].join('\n'), {
-    headers: { 'Content-Type': 'text/plain; charset=utf-8' },
-  });
